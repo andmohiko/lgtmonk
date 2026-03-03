caea28006591184d40c66717c864cb1628d56343
andmohiko <andmohiko@gmail.com>
2026-03-03 22:07:18 +0900

    fix: vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
@@ -9,7 +8,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   plugins: [
-    devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart(),
